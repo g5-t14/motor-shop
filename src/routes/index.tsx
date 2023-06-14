cbcb413690e232358fad6c2dfb13562eddb04a53
Victor Guterres <Vgb1337@gmail.com>
2023-06-14 11:28:06 -0300

    feat: base da página de registro

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,12 +1,14 @@
 import { Route, Routes } from "react-router-dom";
 import { Home } from "../pages/home";
 import { Login } from "../pages/login";
+import { Register } from "../pages/register";
 
 export const RoutesMain = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/login" element={<Login/>}></Route>
+      <Route path="/register" element={<Register/>}></Route>
     </Routes>
   );
 };
