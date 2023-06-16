2ef5cf396c274db5c8554db0be6b8aae6aff314d
Jozhué Beni <jbeeniif@gmail.com>
2023-06-16 11:00:46 -0300

    fix: all ready to up

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,18 @@
 import { RoutesMain } from "./routes";
 import "./index.css";
+import { RegisterProvider } from "./Context/RegisterContext";
 import { Header } from "./components/Header";
 import { CarProvider } from "./providers/CarProvider";
+import { RegisterContext } from "./Context/RegisterContext";
 function App() {
   return (
     <>
-      <Header />
-      <CarProvider>
-        <RoutesMain />
-      </CarProvider>
+      <RegisterProvider>
+        <Header />
+        <CarProvider>
+          <RoutesMain />
+        </CarProvider>
+      </RegisterProvider>
     </>
   );
 }
